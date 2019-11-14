eabfc7c28f08b88e2213eada795fd413d1fc7a12
aca10370eo <kaseikappatokunn2@yahoo.co.jp>
2019-11-14 18:21:14 +0900

    kernel fix

--- jobscript/makescript.py
+++ jobscript/makescript.py
@@ -10,8 +10,8 @@ scripts = []
 for k in glob.glob(os.path.expanduser('~/Documents/umap_mnv2/dataset_all/*')):
     # for i in [0.01, 0.02, 0.05, 0.07, 0.1, 0.2, 0.5, 0.7, 1.0]: # c
     for i in [1.0]:
-        for j in [12]:
-        # for j in range(0, 19, 6):  # layer
+        # for j in [12]:
+        for j in range(0, 19, 6):  # layer
             # for l in [7, 5, 14, 32, 84, 55, 71, 16, 78]:  # seed
             for l in [55]:
                 scriptname = 'josbscript'+str(os.path.basename(k))+'_'+str(i)+'_'+str(j)+'_'+str(l)+'.sh'
