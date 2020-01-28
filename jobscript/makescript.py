303db1a2ee1ea81209eb7dbc59bdf009ff9cb4b6
aca10370eo <kaseikappatokunn2@yahoo.co.jp>
2020-01-28 18:41:05 +0900

    update jobscritp for full paper

--- jobscript/makescript.py
+++ jobscript/makescript.py
@@ -10,18 +10,18 @@ scripts = []
 for k in glob.glob(os.path.expanduser('~/Documents/umap_mvn2/dataset_all/*')):
     # for i in [0.001, 0.005, 0.01, 0.1]: # c
     # for i in [0.01, 0.1, 0.5, 1.0]: # c
-    for i in [0.05]:
+    for i in [0.01]:
         # for j in [9999]:
         # for j in range(0, 19, 6):  # layer
         for j in [19]:
-            for l in [6, 15, 32]:
-            # for l in [6, 5, 15, 32, 85, 55, 71, 16, 78, 69]:  # seed
+            # for l in [6, 15, 32]:
+            for l in [6, 5, 15, 32, 85, 55, 71, 16, 78, 69]:  # seed
                 # for m in [0.000]:
                 # for m in [0.85, 0.9, 0.95, 0.99, 0.999]:
                 for m in [0.999]:
-                    # for mul_sig in [3]:
+                    for mul_sig in [3]:
                     # for mul_sig in [1, 2, 3, 4, 5]:
-                    for mul_sig in [1, 3, 5]:
+                    # for mul_sig in [1, 3, 5]:
                         scriptname = 'josbscript'+str(os.path.basename(k))+'_'+str(i)+'_'+str(j)+'_'+str(l)+'_'+str(m)+'_'+str(mul_sig)+'.sh'
                         scripts.append(scriptname)
                         print(scriptname)
